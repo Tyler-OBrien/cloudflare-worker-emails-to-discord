540bd2fd32de6d9c5dafd9f951c7d745a2a17568
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2023-03-12 16:52:57 -0400

    Fall back to html if no text body (using html-to-text package) Use Header From for Author rather then Envelope & add envelope  from to footer Thanks to @jahands for telling me about these! Also added better following of Discord's embed field limits

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const PostalMime = require("postal-mime");
+const { convert } = require('html-to-text');
 
 // It's 4096
 const DISCORD_EMBED_LIMIT = 4096;
@@ -14,31 +15,36 @@ export default {
     let arrayBuffer = await rawEmail.arrayBuffer();
     const parser = new PostalMime.default();
     const email = await parser.parse(arrayBuffer);
-    
+    let emailText = email.text;
+    if (!emailText) {
+      // If there is no text, try to get the text from the html
+      emailText = convert(email.html);
+    }
+// The overall limit is 6000 characters, and we limit the embed body to 4096 characters, so the rest has ~1900 characters to work with
     let embedBody = JSON.stringify({
       embeds: [
         {
-          title: `${message.headers.get("subject")}`,
+          title: this.trimToLimit(email.subject, 256), // Limit is 256
           description:
-            email.text.length > DISCORD_EMBED_LIMIT
-              ? `${email.text.substring(
+          emailText.length > DISCORD_EMBED_LIMIT
+              ? `${emailText.substring(
                   0,
                   DISCORD_EMBED_LIMIT - 12
                 )}...(TRIMMED)`
-              : email.text,
+              : emailText,
           author: {
-            name: message.from,
+            name: `${(this.trimToLimit(email.from.name, 100))} <${this.trimToLimit(email.from.address, 100)}>`, // Limit of 256 characters, but we will be a bit careful
           },
           footer: {
-            text: `This email was sent to ${message.to}`,
+            text: `This email was sent to ${this.trimToLimit(message.to, 100)}\nEnvelope From: ${this.trimToLimit(message.from, 100)}`, // Limit of 2048 characters, but we will be careful
           },
         },
       ],
     });
     let formData = new FormData();
     formData.append("payload_json", embedBody);
-    if (email.text.length > DISCORD_EMBED_LIMIT) {
-      let newTextBlob = new Blob([email.text], {
+    if (emailText.length > DISCORD_EMBED_LIMIT) {
+      let newTextBlob = new Blob([emailText], {
         type: "text/plain",
       });
       // If the text is too big, we need truncate the blob.
@@ -68,4 +74,7 @@ export default {
     // Or you could do something more complex with adding it to a Queue and retrying sending to Discord, etc
     // For now, I don't really care about those conditions
   },
+  trimToLimit(input, limit) {
+    return input.length > limit ? `${input.substring(0, limit - 12)}...(TRIMMED)` : input;
+  }
 };
